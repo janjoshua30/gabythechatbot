1e79cfbfbae2f91b29a04cd45c613d68d32dd82f
janjoshua30 <33167236+janjoshua30@users.noreply.github.com>
2020-05-31 23:17:16 +0800

    Update index.js

--- gabyadmin/scripts/index.js
+++ gabyadmin/scripts/index.js
@@ -43,8 +43,8 @@ const setupGuides = (data) => {
         <li>
           <div class="collapsible-header green white-text">FROM: ${guide.title} </div>
           <div class="collapsible-body grey lighten-2"> ${guide.content}?
-          <button onclick="location.href='http://www.example.com'">www.example.com</button>
-          <button href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=abeth.cavestany@o365.deped.gov.ph&tf=1" class="btn red darken-2 z-depth-0 modal-trigger" data-target="delete-question">DELETE</button>
+          <a href="https://www.w3schools.com/html/default.asp">HTML tutorial</a>
+          <button href="#" class="btn red darken-2 z-depth-0 modal-trigger" data-target="delete-question">DELETE</button>
           </div>
         </li>
       `;
